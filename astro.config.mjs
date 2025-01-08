f384477e7e07d41694db7d4c2e86f8a694b1bf1e
Thomas Beckford <tebeckford@gmail.com>
2025-01-08 19:34:47 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,11 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config'
+import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://movingout.dev',
   integrations: [tailwind(), react()],
-
-  env: {
-    schema: {
-      TURSO_DATABASE_URL: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-      TURSO_AUTH_TOKEN: envField.string({
-        context: 'server',
-        access: 'secret',
-      }),
-    },
-  },
 })
