2661c4055b0a34d7cc1723f675510b628540687a
Thomas Beckford <tebeckford@gmail.com>
2025-01-10 00:38:44 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
 
 export default defineConfig({
   site: 'https://movingout.dev',
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 })
